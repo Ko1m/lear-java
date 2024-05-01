09911d75dbd8e24c700fdc6caedda964851c50f0
ko1lm <kolmthusday@gmail.com>
2024-05-01 05:05:29 +0300

    First java code

---
+++ Hello.java
@@ -0,0 +1,25 @@
+public class FunWithJava {
+
+    record Cat(String name) {}
+    record Dog(String name) {}
+    record Duck(String name) {}
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("Whiskers");
+        Dog dog = new Dog("Fido");
+        Duck duck = new Duck("Daffy");
+
+        System.out.println(speak(cat));
+        System.out.println(speak(dog));
+        System.out.println(speak(duck));
+    }
+
+    public static String speak(Object animal) {
+        return switch (animal) {
+            case Cat c -> "The cat " + c.name() + " says: Meow!";
+            case Dog d -> "The dog " + d.name() + " says: Woof!";
+            case Duck d -> "The duck " + d.name() + " says: Quack!";
+            default -> "Unknown animal!";
+        };
+    }
+}
